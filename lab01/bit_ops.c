19cb99d899289aaab6a239f461609f2884086771
avl <lechanka@quantstellation.com>
2021-02-03 18:19:16 +0300

    update lab 1

---
+++ lab01/bit_ops.c
@@ -0,0 +1,24 @@
+#include "bit_ops.h"
+
+// Вернуть бит номер n числа x.
+// Считать, что 0 <= n <= 31
+unsigned get_bit(unsigned x,
+                 unsigned n) {
+    // YOUR CODE HERE
+    // Взврат -1 бессмысленный и сделан для правильной компиляции
+    return -1;
+}
+// Выставить значение v в бите номер n
+// Считать, что 0 <= n <= 31 и v равно 0 или 1
+void set_bit(unsigned * x,
+             unsigned n,
+             unsigned v) {
+    // YOUR CODE HERE
+}
+// Сменить значение бита номер n на противоположное.
+// Считать, что 0 <= n <= 31
+void flip_bit(unsigned * x,
+              unsigned n) {
+    // YOUR CODE HERE
+}
+
---
+++ lab01/lfsr.c
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "lfsr.h"
+
+void lfsr_calculate(uint16_t *reg) {
+    /* YOUR CODE HERE */
+}
+
