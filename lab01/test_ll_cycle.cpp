3ca5a2ff5ea75adc0af4cf5d21c786b6365fcac7
avl <lechanka@quantstellation.com>
2021-02-16 20:05:43 +0300

    fixes in output in test_ll

--- lab01/test_ll_cycle.cpp
+++ lab01/test_ll_cycle.cpp
@@ -16,7 +16,7 @@ int main(void) {
     nodes[1].next = &nodes[2];
     nodes[2].next = &nodes[3];
     is_correct = !ll_has_cycle(&nodes[0]);
-    std::cout << "Тест 1. Циклов нет. Резултат ll_has_cycle: " << std::boolalpha << ll_has_cycle(&nodes[0]) << std::endl;
+    std::cout << "Тест 1. Циклов нет. Результат ll_has_cycle: " << std::boolalpha << ll_has_cycle(&nodes[0]) << std::endl;
 
     nodes[4].next = &nodes[5];
     nodes[5].next = &nodes[6];
@@ -26,7 +26,7 @@ int main(void) {
     nodes[9].next = &nodes[10];
     nodes[10].next = &nodes[4];
     is_correct = is_correct && ll_has_cycle(&nodes[4]);
-    std::cout << "Тест 2. Циклы есть. Резултат ll_has_cycle: " << std::boolalpha << ll_has_cycle(&nodes[0]) << std::endl;
+    std::cout << "Тест 2. Циклы есть. Результат ll_has_cycle: " << std::boolalpha << ll_has_cycle(&nodes[4]) << std::endl;
 
     nodes[11].next = &nodes[12];
     nodes[12].next = &nodes[13];
@@ -36,21 +36,21 @@ int main(void) {
     nodes[16].next = &nodes[17];
     nodes[17].next = &nodes[14];
     is_correct = is_correct && ll_has_cycle(&nodes[11]);
-    std::cout << "Тест 3. Циклы есть. Резултат ll_has_cycle: " << std::boolalpha << ll_has_cycle(&nodes[0]) << std::endl;
+    std::cout << "Тест 3. Циклы есть. Результат ll_has_cycle: " << std::boolalpha << ll_has_cycle(&nodes[11]) << std::endl;
 
     nodes[18].next = &nodes[18];
     is_correct = is_correct && ll_has_cycle(&nodes[18]);
-    std::cout << "Тест 4. Циклы есть. Резултат ll_has_cycle: " << std::boolalpha << ll_has_cycle(&nodes[0]) << std::endl;
+    std::cout << "Тест 4. Циклы есть. Результат ll_has_cycle: " << std::boolalpha << ll_has_cycle(&nodes[18]) << std::endl;
 
     nodes[19].next = &nodes[20];
     nodes[20].next = &nodes[21];
     nodes[21].next = &nodes[22];
     nodes[22].next = &nodes[23];
     is_correct = is_correct && !ll_has_cycle(&nodes[19]);
-    std::cout << "Тест 5. Циклов нет. Резултат ll_has_cycle: " << std::boolalpha << ll_has_cycle(&nodes[0]) << std::endl;
+    std::cout << "Тест 5. Циклов нет. Результат ll_has_cycle: " << std::boolalpha << ll_has_cycle(&nodes[19]) << std::endl;
 
-    is_correct = is_correct && !ll_has_cycle(NULL);
-    std::cout << "Тест 5. Циклов нет. Резултат ll_has_cycle: " << std::boolalpha << ll_has_cycle(&nodes[0]) << std::endl;
+    is_correct = is_correct && !ll_has_cycle(nullptr);
+    std::cout << "Тест 5. Циклов нет. Результат ll_has_cycle: " << std::boolalpha << ll_has_cycle(nullptr) << std::endl;
 
     std::cout << (is_correct ? "Успех\n": "Провал\n");
     return is_correct ? 0 : 1;
