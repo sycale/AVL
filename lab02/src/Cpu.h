13d883bc2ecffb55d831149569ce8f4432ec5e6f
avl <lechanka@quantstellation.com>
2021-02-21 14:13:24 +0300

    new lab

---
+++ lab02/src/Cpu.h
@@ -0,0 +1,46 @@
+
+#ifndef RISCV_SIM_CPU_H
+#define RISCV_SIM_CPU_H
+
+#include "Memory.h"
+#include "Decoder.h"
+#include "RegisterFile.h"
+#include "CsrFile.h"
+#include "Executor.h"
+
+class Cpu
+{
+public:
+    Cpu(Memory& mem)
+        : _mem(mem)
+    {
+
+    }
+
+    void ProcessInstruction()
+    {
+        /* YOUR CODE HERE */
+    }
+
+    void Reset(Word ip)
+    {
+        _csrf.Reset();
+        _ip = ip;
+    }
+
+    std::optional<CpuToHostData> GetMessage()
+    {
+        return _csrf.GetMessage();
+    }
+
+private:
+    Reg32 _ip;
+    Decoder _decoder;
+    RegisterFile _rf;
+    CsrFile _csrf;
+    Executor _exe;
+    Memory& _mem;
+};
+
+
+#endif //RISCV_SIM_CPU_H
---
+++ lab02/src/Executor.h
@@ -0,0 +1,20 @@
+
+#ifndef RISCV_SIM_EXECUTOR_H
+#define RISCV_SIM_EXECUTOR_H
+
+#include "Instruction.h"
+
+class Executor
+{
+public:
+    void Execute(InstructionPtr& instr, Word ip)
+    {
+        /* YOUR CODE HERE */
+    }
+
+private:
+    /* YOUR CODE HERE */
+
+};
+
+#endif // RISCV_SIM_EXECUTOR_H
